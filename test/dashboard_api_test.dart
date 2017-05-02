4138a81d8713101a4bc28c093fcdb8d4acd4835b
passerbyflutter <passerbyflutter@infinitiessoft.com>
2017-05-02 16:12:48 +0800

    no message

--- test/dashboard_api_test.dart
+++ test/dashboard_api_test.dart
@@ -1,5 +1,5 @@
-import 'dart:async';
 import 'dart:convert';
+
 import 'package:cf_mock_api/dashboard_api.dart';
 import 'package:test/test.dart';
 
@@ -13,7 +13,8 @@ main() {
 
   test('test test', () async {
     List<Map<String, String>> target = await dashboard.getDashboardList();
-    expect(JSON.encode(target), '[{"location":"Tokyo","vmCount":2,"rdsCount":1,"slbCount":2,"recentlyCount":2,"expireCount":0},{"location":"Singapore","vmCount":3,"rdsCount":2,"slbCount":2,"recentlyCount":1,"expireCount":1},{"location":"Taiwan","vmCount":1,"rdsCount":0,"slbCount":0,"recentlyCount":1,"expireCount":0}]');
+    expect(JSON.encode(target),
+        '[{"location":"Tokyo","vmCount":2,"rdsCount":1,"slbCount":2,"recentlyCount":2,"expireCount":0},{"location":"Singapore","vmCount":3,"rdsCount":2,"slbCount":2,"recentlyCount":1,"expireCount":1},{"location":"Taiwan","vmCount":1,"rdsCount":0,"slbCount":0,"recentlyCount":1,"expireCount":0}]');
   });
 
 
