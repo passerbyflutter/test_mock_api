180eefffc2136f3708aee35c93018568e5a9f952
passerbyflutter <passerbyflutter@infinitiessoft.com>
2017-05-04 15:21:19 +0800

    right

--- test/dashboard_api_test.dart
+++ test/dashboard_api_test.dart
@@ -14,7 +14,7 @@ main() {
   test('test test', () async {
     List<Map<String, String>> target = await dashboard.getDashboardList();
     expect(JSON.encode(target),
-        '[{"location":"Tokyo","vmCount":2,"rdsCount":1,"slbCount":2,"recentlyCount":2,"expireCount":0},{"location":"Singa2,"slbCount":2,"recentlyCount":1,"expireCount":1},{"location":"Taiwan","vmCount":1,"rdsCount":0,"slbCount":0,"recentlyCount":1,"expireCount":0}]');
+        '[{"location":"Tokyo","vmCount":2,"rdsCount":1,"slbCount":2,"recentlyCount":2,"expireCount":0},{"location":"Singapore","vmCount":3,"rdsCount":2,"slbCount":2,"recentlyCount":1,"expireCount":1},{"location":"Taiwan","vmCount":1,"rdsCount":0,"slbCount":0,"recentlyCount":1,"expireCount":0}]');
   });
 
 
